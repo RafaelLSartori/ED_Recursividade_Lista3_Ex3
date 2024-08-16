67c2fbdca43d1c1bf28c905667befff8c3a9187c
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-16 12:18:12 -0300

    Atualização

--- src/controller/Ex3.java
+++ src/controller/Ex3.java
@@ -1,2 +1,17 @@
-package controller;public class Ex3 {
+package controller;
+
+public class Ex3 {
+    public Ex3(){
+        super();
+    }
+    public String frecinvertepalavra(String palavra, int tamanho){
+        if (tamanho == 1) {
+            //A condição de parada foi realizada pensando no primeira letra da palavra. Com a recursiva eu vou excluindo os demais
+            // caracteres e paro quando chegar na última letra, fazendo com que ela seja a primeira letra.
+            return palavra;
+        }
+        //Aqui chamo a recursiva excluindo a primeira letra e vou diminuindo o tamanho da palavra. O charAt retorna o caractere na posição 0.
+        return frecinvertepalavra(palavra.substring(1), tamanho -1) + palavra.charAt(0);
+
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,14 @@
+package view;
+
+import controller.Ex3;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex3 m = new Ex3();
+        String palavra = JOptionPane.showInputDialog("Digite a palavra a ser invertida: ");
+        int tamanho = palavra.length();
+        System.out.println(m.frecinvertepalavra(palavra, tamanho));
     }
 }
